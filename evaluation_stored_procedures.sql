04153ac8b69e185829bb34184be34e7a4eef05e1
CreamSerenus <cseanwyatt@gmail.com>
2022-11-30 16:22:00 +0800

    Update evaluation_stored_procedures.sql

--- evaluation_stored_procedures.sql
+++ evaluation_stored_procedures.sql
@@ -23,7 +23,7 @@ DELIMITER //
 Create Procedure evaluate_teacher(IN stu int, IN t_num int, IN c_code int, in f_code varchar(128), in q1 int, in q2 int, in q3 int, in q4 int, in q5 int, in q6 int, in q7 int, in q8 int, in q9 int, in q10 int)
 	Begin
 		Insert into eval_status (stu_num, teachers_code, course_code, eval_date, form_code) values (stu, t_num, c_code, curdate(), f_code);
-        Insert into forms(form_code, teachers_code, c_code, q1_score, q2_score, q3_score, q4_score, q5_score, q6_score, q7_score, q8_score, q9_score, q10_score) values (f_code, t_num, q1,q2,q3,q4,q5,q6,q7,q8,q9,q10);
+        Insert into forms(form_code, teachers_code, course_code, q1_score, q2_score, q3_score, q4_score, q5_score, q6_score, q7_score, q8_score, q9_score, q10_score) values (f_code, t_num, c_code, q1,q2,q3,q4,q5,q6,q7,q8,q9,q10);
         commit;
     End
 DELIMITER ;

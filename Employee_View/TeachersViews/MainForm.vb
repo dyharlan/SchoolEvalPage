3e2cd9a3ccfedeaf393b54a064ccd4e5f3732971
Minshio <118647040+Minshio@users.noreply.github.com>
2022-12-04 23:16:30 +0800

    Readded earlier updates

--- Employee_View/TeachersViews/MainForm.vb
+++ Employee_View/TeachersViews/MainForm.vb
@@ -187,4 +187,8 @@ Public Class MainForm
     Private Sub MainForm_Shown(sender As Object, e As EventArgs) Handles Me.Shown
         isMenuEnabled(False)
     End Sub
+
+    Private Sub EvalStatusToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EvalStatusToolStripMenuItem.Click
+        EvalScoreView.ShowDialog()
+    End Sub
 End Class

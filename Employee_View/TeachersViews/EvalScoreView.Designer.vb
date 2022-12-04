0b29af4486962fd0f478bf4f52cf26d74c051762
Minshio <118647040+Minshio@users.noreply.github.com>
2022-12-04 19:34:49 +0800

    Added eval score viewing for teacher logged in

--- Employee_View/TeachersViews/EvalScoreView.Designer.vb
+++ Employee_View/TeachersViews/EvalScoreView.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class EvalScoreView
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,89 +20,73 @@ Partial Class EvalScoreView
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.btnSort = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(42, 109)
+        Me.DataGridView1.Location = New System.Drawing.Point(24, 66)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowTemplate.Height = 25
-        Me.DataGridView1.Size = New System.Drawing.Size(797, 290)
+        Me.DataGridView1.Size = New System.Drawing.Size(699, 101)
         Me.DataGridView1.TabIndex = 0
         '
-        'Label1
+        'Button1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(48, 44)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(68, 15)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Sort Option"
+        Me.Button1.Location = New System.Drawing.Point(24, 186)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Go Back"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(129, 44)
+        Me.ComboBox1.Location = New System.Drawing.Point(70, 35)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 2
         '
-        'RadioButton1
+        'Label1
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Checked = True
-        Me.RadioButton1.Location = New System.Drawing.Point(265, 46)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(76, 19)
-        Me.RadioButton1.TabIndex = 3
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Tag = ""
-        Me.RadioButton1.Text = "Acending"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Course:"
         '
-        'RadioButton2
+        'Button2
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(347, 46)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(82, 19)
-        Me.RadioButton2.TabIndex = 4
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "Decending"
-        Me.RadioButton2.UseVisualStyleBackColor = True
-        '
-        'btnSort
-        '
-        Me.btnSort.Location = New System.Drawing.Point(442, 46)
-        Me.btnSort.Name = "btnSort"
-        Me.btnSort.Size = New System.Drawing.Size(75, 23)
-        Me.btnSort.TabIndex = 5
-        Me.btnSort.Text = "Sort"
-        Me.btnSort.UseVisualStyleBackColor = True
+        Me.Button2.Location = New System.Drawing.Point(209, 33)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(81, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Show Scores"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'EvalScoreView
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(875, 453)
-        Me.Controls.Add(Me.btnSort)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.ClientSize = New System.Drawing.Size(750, 233)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "EvalScoreView"
-        Me.Text = "EvalScoreView"
+        Me.Text = "Evaluation Scores"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -110,9 +94,8 @@ Partial Class EvalScoreView
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
     Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
-    Friend WithEvents btnSort As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
 End Class
--- Employee_View/TeachersViews/EvalScoreView.vb
+++ Employee_View/TeachersViews/EvalScoreView.vb
@@ -1,3 +1,73 @@
-﻿Public Class EvalScoreView
+﻿Imports MySql.Data.MySqlClient
+Imports Mysqlx.XDevAPI.Relational
 
+Public Class EvalScoreView
+    Dim sqlConn As New MySqlConnection
+    Dim sqlCmd As New MySqlCommand
+    Dim sqlRd As MySqlDataReader
+    Dim sqlDt As New DataTable
+    Dim assign As String = "SELECT * FROM evaluation.course_offerings"
+    Dim holder As New Integer
+    Dim holder1 As New Integer
+    Public holder2 As String
+
+    'Dim Server As String = "server=localhost;user=root;password=password;port=3310;database=evaluation"
+
+    Private Sub EvalScoreView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        holder1 = CType(holder2, Integer)
+        comboBoxLoad()
+        loadTable()
+    End Sub
+
+    Private Sub loadTable()
+        sqlDt.Clear()
+        DataGridView1.DataSource = sqlDt
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Refresh()
+
+        sqlConn.ConnectionString = Main.SERVER_NAME
+
+        sqlConn.Open()
+        sqlCmd.Connection = sqlConn
+        sqlCmd.CommandText = "call check_eval_scores('" & holder1 & "','" & holder & "');"
+
+        sqlRd = sqlCmd.ExecuteReader
+
+        sqlDt.Load(sqlRd)
+        sqlRd.Close()
+        sqlConn.Close()
+        DataGridView1.DataSource = sqlDt
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub comboBoxLoad()
+        Dim sqlCon As New MySqlConnection
+        Dim sqlCom As New MySqlCommand(assign, sqlCon)
+        sqlCon.ConnectionString = Main.SERVER_NAME
+
+        sqlCon.Open()
+        Dim sqlTbl As New DataTable()
+        sqlTbl.Load(sqlCom.ExecuteReader)
+        ComboBox1.DataSource = sqlTbl
+        ComboBox1.DisplayMember = "COURSE_CODE"
+        sqlConn.Close()
+
+        If ComboBox1.Items.Count > 0 Then
+            ComboBox1.SelectedIndex = 0
+        End If
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        holder = CType(ComboBox1.Text, Integer)
+        loadTable()
+    End Sub
 End Class

f974acd46e036f6025686032c0b53cb1ccaea598
Jesun128 <111490001+Jesun128@users.noreply.github.com>
2022-12-04 18:14:21 +0800

    Add files via upload

---
+++ Employee_View/TeachersViews/class/Department.vb
@@ -0,0 +1,202 @@
+﻿Imports System.Text
+Imports MySql.Data.MySqlClient
+
+Public Class Department
+    Private deptID As Integer
+    Private deptName As String
+    Dim db As Db = New Db()
+    Private tableName As String = TABLE_DEPARTMENTS
+
+    Public Sub New()
+    End Sub
+    Public Sub New(deptID As Integer, deptName As String)
+        Me.deptID = deptID
+        Me.deptName = deptName
+    End Sub
+
+    Public Property DeptID1 As Integer
+        Get
+            Return deptID
+        End Get
+        Set(value As Integer)
+            deptID = value
+        End Set
+    End Property
+
+    Public Property DeptName1 As String
+        Get
+            Return deptName
+        End Get
+        Set(value As String)
+            deptName = value
+        End Set
+    End Property
+
+
+    Public Overloads Function save() As Integer
+
+
+        Dim conn As MySqlConnection = db.getMySqlConnection()
+        Dim tran As MySqlTransaction = conn.BeginTransaction
+
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+
+        Try
+            Dim sql As StringBuilder = New StringBuilder()
+            sql.Append("INSERT INTO " & tableName & " ")
+            sql.Append("(")
+
+            sql.Append("DEPT_ID,")
+            sql.Append("DEPT_NAME")
+
+            sql.Append(")")
+            sql.Append("VALUES")
+            sql.Append("(")
+
+            sql.Append("@deptId,")
+            sql.Append("@deptName")
+
+            sql.Append(");")
+
+            Dim cmd As New MySqlCommand
+            cmd.Transaction = tran
+            cmd.Connection = conn
+            cmd.CommandText = sql.ToString
+            cmd.Parameters.AddWithValue("@deptId", Me.DeptID1)
+            cmd.Parameters.AddWithValue("@deptName", Me.DeptName1)
+
+            cmd.ExecuteNonQuery()
+            tran.Commit()
+
+            conn.Close()
+        Catch ex As Exception
+            tran.Rollback()
+            MsgBox("Error occured: Could not insert record: " & ex.Message)
+            Return 0
+        End Try
+
+        Return 1
+    End Function
+
+    Public Overloads Function update(_deptId As Integer) As Integer
+
+        Dim conn = New Db().getMySqlConnection()
+        Dim tran As MySqlTransaction = conn.BeginTransaction
+
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+
+
+        Try
+            Dim sql As StringBuilder = New StringBuilder()
+            sql.Append("UPDATE " & tableName & " ")
+            sql.Append("SET ")
+
+            sql.Append("DEPT_ID=@deptId,")
+            sql.Append("DEPT_NAME=@deptName")
+
+            sql.Append(" WHERE DEPT_ID=@_deptId")
+
+            Dim cmd As New MySqlCommand
+            cmd.Transaction = tran
+            cmd.Connection = conn
+            cmd.CommandText = sql.ToString
+            cmd.Parameters.AddWithValue("@deptId", Me.DeptID1)
+            cmd.Parameters.AddWithValue("@deptName", Me.DeptName1)
+            cmd.Parameters.AddWithValue("@_deptId", _deptId)
+
+            cmd.ExecuteNonQuery()
+            tran.Commit()
+
+            conn.Close()
+
+        Catch ex As Exception
+            tran.Rollback()
+            MsgBox("Error occured: Could not update record: " & ex.Message)
+            Return 0
+        End Try
+
+        Return 1
+    End Function
+
+    Public Overloads Function delete() As Integer
+
+        Dim conn = New Db().getMySqlConnection()
+        Dim tran As MySqlTransaction = conn.BeginTransaction
+
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+
+        Try
+            Dim sql As StringBuilder = New StringBuilder()
+            sql.Append("DELETE FROM " & tableName & " ")
+            sql.Append(" WHERE DEPT_ID=@deptId")
+
+            Dim cmd As New MySqlCommand
+            cmd.Transaction = tran
+            cmd.Connection = conn
+            cmd.CommandText = sql.ToString
+            cmd.Parameters.AddWithValue("@deptId", DeptID1)
+            cmd.ExecuteNonQuery()
+
+
+            tran.Commit()
+            conn.Close()
+
+        Catch ex As Exception
+            tran.Rollback()
+            MsgBox("Error occured: Could not update record: " & ex.Message)
+            Return 0
+        End Try
+
+        Return 1
+    End Function
+
+    Public Overloads Function all() As BindingSource
+
+        Dim bs1 As New BindingSource
+        Try
+            Dim conn = New Db().getMySqlConnection
+
+            Dim sql As StringBuilder = New StringBuilder
+            sql.Append("SELECT a.DEPT_ID, a.DEPT_NAME FROM " & tableName & " a ")
+            sql.Append(" ORDER BY a.DEPT_NAME asc")
+
+            Dim da As New MySqlDataAdapter(sql.ToString, conn)
+            Dim ds As DataSet = New DataSet
+            da.Fill(ds, tableName)
+            bs1.DataSource = ds.Tables(tableName)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return bs1
+
+    End Function
+
+    Public Function lookUpDepartment() As BindingSource
+
+        Dim prodId As Integer = 0
+        Dim bs1 As New BindingSource
+        Try
+            Dim conn As MySqlConnection = db.getMySqlConnection()
+            Dim Str As String = "SELECT * FROM " & tableName
+            Dim Search As New MySqlDataAdapter(Str, conn)
+            Dim ds As DataSet = New DataSet
+            Search.Fill(ds, tableName)
+            bs1.DataSource = ds.Tables(tableName)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return bs1
+
+    End Function
+
+End Class
---
+++ Employee_View/TeachersViews/form template/ClassAssignmentDetail.vb
@@ -0,0 +1,130 @@
+﻿Imports System.ComponentModel.Design
+Imports System.Dynamic
+Imports Microsoft.VisualBasic.FileIO
+Imports Mysqlx.XDevAPI.Relational
+Imports Org.BouncyCastle.Crypto.Engines
+
+Public Class ClassAssignmentDetail
+    Dim classAssignmentBS As New BindingSource
+    Private rowPosition As Integer
+    Sub init()
+        loadStudent()
+        loadClass()
+        loadClassAssignment()
+    End Sub
+
+    Sub loadStudent()
+        Dim student As New Student
+        Dim studentBs As BindingSource = student.lookUpStudent
+        studentBs.AddNew()
+        Dim newRow = studentBs.Current
+        newRow.item("STU_NUM") = 0
+        newRow.item("STUDENT_NAME") = "Select Student"
+
+        Me.cmbStudent.DisplayMember = "STUDENT_NAME"
+        Me.cmbStudent.ValueMember = "STU_NUM"
+        Me.cmbStudent.DataSource = studentBs
+
+
+
+    End Sub
+    Sub loadClass()
+        Dim schoolclass As New SchoolClass
+        Dim classBs As BindingSource = schoolclass.lookUpClass
+        classBs.AddNew()
+        Dim newRow = classBs.Current
+        newRow.item("CLASS_CODE") = 0
+        newRow.item("CLASS_NAME") = "Select Class"
+
+        Me.cmbClass.DisplayMember = "CLASS_NAME"
+        Me.cmbClass.ValueMember = "CLASS_CODE"
+        Me.cmbClass.DataSource = classBs
+    End Sub
+    Private Sub ClassAssignmentDetail_Load(sender As Object, e As EventArgs) Handles Me.Load
+        init()
+    End Sub
+
+    Private Sub cmbClass_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbClass.SelectedIndexChanged
+
+    End Sub
+    Sub loadClassAssignment()
+        Dim classAssignment As New ClassAssignment
+        classAssignmentBS = classAssignment.FILL_BY_STU_NUM()
+        Me.DataGridView1.DataSource = classAssignmentBS
+        classAssignmentBS.Position = rowPosition
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If Me.cmbStudent.Text = Nothing Or Me.cmbClass.Text = Nothing Then
+            MsgBox("Student and Class cannot be empty.", MsgBoxStyle.Critical)
+            Return
+        End If
+
+
+        Dim stunum As Integer
+        Dim classCode As Integer
+        Dim result As Integer
+
+        stunum = Me.cmbStudent.SelectedValue
+        classCode = Me.cmbClass.SelectedValue
+
+        If stunum = 0 Or classCode = 0 Then
+            MsgBox("Student and Class cannot be empty.", MsgBoxStyle.Critical)
+            Return
+        End If
+
+        Dim classassignment As New ClassAssignment
+        classassignment.StudentNumber1 = stunum
+        classassignment.ClassCode1 = classCode
+        result = classassignment.save()
+        If result > 0 Then
+            loadClassAssignment()
+            Me.cmbStudent.SelectedValue = 0
+            Me.cmbClass.SelectedValue = 0
+        End If
+    End Sub
+
+    Private Sub cmbStudent_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbStudent.SelectedIndexChanged
+        'If Me.cmbStudent.Text = Nothing Then
+        '    Return
+        'End If
+        'Dim val = Me.cmbStudent.SelectedValue
+        'loadClassAssignment()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If classAssignmentBS.Count > 0 Then
+            Dim row = classAssignmentBS.Current
+            If MsgBox("Delete class assignment for " & row.Item("STUDENT") & " ?", MsgBoxStyle.YesNo) = vbYes Then
+
+                Dim ca As New ClassAssignment
+                ca.ClassCode1 = row.Item("CLASS_CODE")
+                ca.StudentNumber1 = row.Item("STU_NUM")
+                If ca.delete > 0 Then
+                    classAssignmentBS.RemoveCurrent()
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim classDetail As New Class_Detail
+        classDetail.addNew()
+        classDetail.ShowDialog()
+        loadClass()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim studentDetail As New StudentDetail
+        studentDetail.addNew()
+        studentDetail.ShowDialog()
+        loadStudent()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+End Class
